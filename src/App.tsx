65173a5b5c57797bde4145284620d88f5bc822d9
Lemo <o.lemonakis@live.dk>
2025-03-23 18:59:58 +0100

    fixed vite shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { useState } from 'react'
-import { Heading, Box, Center, AbsoluteCenter, Theme, Color } from "@chakra-ui/react"
-import { ColorModeProvider } from "./components/ui/color-mode"
-import BackgroundPattern from "./components/background-pattern"
-import TestCard from './components/test'
 
+import './App.css'
 
 function App() {
-
   return (
-    <Box bgColor='blackAlpha.900' width='100vw' height='100vh'>    
-      <AbsoluteCenter>
-        <TestCard/>
-      </AbsoluteCenter>
-      </Box>
-
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
