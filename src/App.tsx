2be490ee58412e78fc7ff0e57085427c7acd040b
Lemo <o.lemonakis@live.dk>
2025-03-29 19:46:36 +0100

    Added hero ui and tailwind to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
 
-import './App.css'
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
