1efca6a8b1ac23aa7698e6f030860e60e63689d9
Lemo <o.lemonakis@live.dk>
2025-05-16 16:05:25 +0200

    New text for mygrade

---
+++ components/projects/mygrade.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function myGrade() {
+  return (
+    <>
+        <div className="h-full flex flex-col items-center  bg-[#f5f0e8] p-2">
+            <h1 className="text-2xl  text-[#8b4513] font-semibold retro-text mb-3">
+                MyGrade, an easier way to GPA
+            </h1>
+                {/* <img
+                  src="/images/bachelor_pic_1.png"
+                  alt="Bachelor Project"
+                  style={{ maxWidth: "100%", height: "auto" }}
+                /> */}
+                <span>Technologies</span>
+                <span>.NET 8, PdfPig, MudBlazor</span>
+            <p className=" text-lg retro-text mt-2 mb-2">
+                Having had the need to provide my GPA, on more than one occasion, I found it to be a quite tedious
+                process calculating it either by hand, or with tools that would require you to input all the data
+                by hand, negating much of the use of the tool. I wanted to change that, and give students a tool
+                which would be enable them to simply upload a PDF transcript, and get their GPA back in multiple scales.
+                <br/>
+                <br/>
+            </p>
+            <h2 className="text-xl  text-[#8b4513] font-retro italic -mb3">Underneath is an excerpt from the thesis.</h2>
+            <p className="text-lg retro-text mb-2">
+                Using PdfPig I was able to parse through PDF transcripts, experimenting with the different methods
+                of segmentation available, printing the segmentation methods onto the transcript and trying to see
+                if I had any way of parsing through said segmentation to any meaningful degree.
+                <br/>
+                ... 
+                <br/>
+                Once I found a way to parse
+            </p>
+            <a className='mt-4  text-[#8b4513] text-xl font-bold underline font-retro' href='/reports/bachelor_report.pdf' target='_blank'>Github Repo</a>
+        </div>
+    </>
+  )
+}
