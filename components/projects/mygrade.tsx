8e1036fdee697fe30555d6fc867e10b7690ec86c
Lemo <o.lemonakis@live.dk>
2025-05-16 16:09:19 +0200

    Text remedied

--- components/projects/mygrade.tsx
+++ components/projects/mygrade.tsx
@@ -12,28 +12,28 @@ export default function myGrade() {
                   alt="Bachelor Project"
                   style={{ maxWidth: "100%", height: "auto" }}
                 /> */}
+                <span>Technologies</span>
+                <span>.NET 8, PdfPig, MudBlazor</span>
             <p className=" text-lg retro-text mt-2 mb-2">
-                Our thesis concerned the use of eye-tracking in VR-gaming. 
-                The purpose was to mimic eye-tracking interactions, without the use of eye-tracking technology. 
-                We called this vision-based interactions.
-                The project was built with C# and the Unity game engine, with the use of a Meta Quest 2.
+                Having had the need to provide my GPA, on more than one occasion, I found it to be a quite tedious
+                process calculating it either by hand, or with tools that would require you to input all the data
+                by hand, negating much of the use of the tool. I wanted to change that, and give students a tool
+                which would be enable them to simply upload a PDF transcript, and get their GPA back in multiple scales.
                 <br/>
                 <br/>
             </p>
-            <h2 className="text-xl  text-[#8b4513] font-retro italic -mb3">Underneath is an excerpt from the thesis.</h2>
+            <h2 className="text-xl  text-[#8b4513] font-retro italic -mb3">Techology and Stack</h2>
             <p className="text-lg retro-text mb-2">
-                "Gaze interaction in video games, using eye tracking, is in growth. 
-                No longer only used for novelty interaction but also as an integral part of the game’s mechanics. 
-                However, while gaze interaction is becoming more and more prevalent, gaze aversion appears to continue in novelty. 
-                Within this paper, we explore the dichotomy of ”see” and ”not see”(look, don’t look”) interactions with what we define as 
-                ”vision-based interactions” as an alternative to traditional eye-tracking gaze interaction for VR horror game elements 
+                Having a lot of experience with C# and Blazor professionally, when I wanted to do a quick and easy proof
+                of concept for an idea, I initially looked for existing PDF parsing libraries in C#. 
                 <br/>
-                ... 
-                <br/>
-                Our results suggest that we might be capable of expecting different interactions with differing intricacies to 
-                elicit stronger responses from users to VR horror game elements.""
+                Using PdfPig I was able to parse through PDF transcripts, experimenting with the different methods
+                of segmentation available, printing the segmentation methods onto the transcript and trying to see
+                if I had any way of parsing through said segmentation to any meaningful degree.Having tried and found a
+                successful way to parse the transcript PDF with PdfPig, I extended the solution with a Blazor web application and
+                quickly prototyped a GUI.
             </p>
-            <a className='mt-4  text-[#8b4513] text-xl font-bold underline font-retro' href='/reports/bachelor_report.pdf' target='_blank'>Download the report</a>
+            <a className='mt-4  text-[#8b4513] text-xl font-bold underline font-retro' href='https://github.com/llemmoo/mygrade' target='_blank'>Github Repo</a>
         </div>
     </>
   )
